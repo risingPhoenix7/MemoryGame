b3065f455840ebfb7a977019a2dce40321a91da3
risingPhoenix7 <achinthyahebbar@gmail.com>
2022-03-06 12:53:35 +0530

    Third commit

--- app/src/main/java/com/example/memoryapp/MainActivity.kt
+++ app/src/main/java/com/example/memoryapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.memoryapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,10 @@ class MainActivity : AppCompatActivity() {
         rvBoard=findViewById(R.id.rvboard)
         tvNumMoves=findViewById(R.id.moves_text)
         tvNumPairs=findViewById(R.id.pairs_text)
+        rvBoard.adapter=MemoryBoardAdapter(this,8)  //MemoryBoardAdapter is the new class that adapts data into required format
+        rvBoard.setHasFixedSize(true)               //optional, just for efficiency
+        rvBoard.layoutManager=GridLayoutManager(this,2)
+        //layout manager built in function to get a layout of spanCount columns
 
     }
 }
--- app/src/main/java/com/example/memoryapp/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/memoryapp/MemoryBoardAdapter.kt
@@ -1,2 +1,27 @@
-package com.example.memoryapp 
+package com.example.memoryapp
 
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoryBoardAdapter(private val context: Context, private val numPieces: Int)
+    : RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>()
+//instead of subclass being RecyclerView.ViewHolder , we created a subclass called ViewHolder and them
+//put that as subclass
+{
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
